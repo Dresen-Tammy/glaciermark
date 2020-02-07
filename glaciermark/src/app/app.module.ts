f1de408b613be7a1675316e20cb12fd27dc453bf
Tammy Dresen <dre15003@byui.edu>
2020-02-07 10:09:12 -0800

    Added firestore

--- glaciermark/src/app/app.module.ts
+++ glaciermark/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
 import { ProjectModule } from './project/project.module';
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { ContactModule } from './contact/contact.module';
@@ -10,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,6 +23,9 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
     AboutModule,
     ServicesPageModule,
     ContactModule,
