e4b40aac84e5190db7278a3498bf06dd6905651c
Tammy Dresen <dre15003@byui.edu>
2020-02-07 21:13:30 -0800

    added footer and updated nav

---
+++ glaciermark/src/app/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.less']
+})
+export class FooterComponent implements OnInit {
+  @Input() navigation: {};
+
+  public constructor() { }
+
+  public ngOnInit() {}
+
+}
