ceb553623b818e50edfc4ccdc201db0b26a7e01d
Tammy Dresen <dre15003@byui.edu>
2020-02-26 16:55:06 -0800

    moved animation to animations.ts

--- glaciermark/src/app/app.component.ts
+++ glaciermark/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { menuAnimation } from './animations';
 import { DataService } from './services/data/data.service';
 import { Subscription } from 'rxjs';
 import { SeoService } from './services/seo/seo.service';
@@ -9,53 +10,9 @@ import { trigger, state, style, animate, transition, query, group, keyframes } f
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
-  animations: [trigger('menuAnimation', [
-    state('open', style({})),
-    state('close', style({})),
-    transition('open => close', [
-      group([
-      query('.icon1', animate('.5s', keyframes([
-        style({ width: '24px', transform: 'translate(0px, 20px) rotate(0.125turn)', offset: 0}),
-        style({ width: '24px', transform: 'translate(0px, 20px) rotate(0turn)', offset: .33}),
-        style({ width: '24px', transform: 'translate(0px, 12px) rotate(0turn)', offset: .67}),
-        style({ width: '18px', transform: 'translate(0px, 12px) rotate(0turn)', offset: 1})
-      ]))),
-      query('.icon2', animate('.5s', keyframes([
-        style({transform: 'translate(0px, 20px) rotate(-.125turn)', offset: 0}, ),
-        style({transform: 'translate(0px, 20px) rotate(0turn)', offset: .33}, ),
-        style({transform: 'translate(0px, 20px) rotate(0turn)', offset: 1}, )
-      ]))),
-      query('.icon3', animate('.5s', keyframes([
-        style({ width: '24px', transform: 'translate(0px, 20px) rotate(-.125turn)', offset: 0}),
-        style({ width: '24px', transform: 'translate(0px, 20px) rotate(0turn)', offset: .33}),
-        style({ width: '24px', transform: 'translate(0px, 28px) rotate(0turn)', offset: .67}),
-        style({ width: '12px', transform: 'translate(0px, 28px) rotate(0turn)', offset: 1})
-      ]))),
-    ]),
-  ]),
-    transition('close => open', [
-      group([
-        query('.icon1', animate('.5s', keyframes([
-          style({ width: '18px', transform: 'translate(0px, 12px) rotate(0turn)', offset: 0}),
-          style({ width: '24px', transform: 'translate(0px, 12px) rotate(0turn)', offset: .33}),
-          style({ width: '24px', transform: 'translate(0px, 20px) rotate(0turn)', offset: .67}),
-          style({ width: '24px', transform: 'translate(0px, 20px) rotate(0.125turn)', offset: 1})
-        ]))),
-        query('.icon2', animate('.5s', keyframes([
-          style({transform: 'translate(0px, 20px) rotate(0turn)', offset: 0}, ),
-          style({transform: 'translate(0px, 20px) rotate(0turn)', offset: .67}, ),
-          style({transform: 'translate(0px, 20px) rotate(-.125turn)', offset: 1}, )
-        ]))),
-        query('.icon3', animate('.5s', keyframes([
-          style({ width: '12px', transform: 'translate(0px, 28px) rotate(0turn)', offset: 0}),
-          style({ width: '24px', transform: 'translate(0px, 28px) rotate(0turn)', offset: .33}),
-          style({ width: '24px', transform: 'translate(0px, 20px) rotate(0turn)', offset: .67}),
-          style({ width: '24px', transform: 'translate(0px, 20px) rotate(-.125turn)', offset: 1})
-        ]))),
-      ])
-  ])
-  ])]
-})
+  animations: [menuAnimation]
+  })
+
 export class AppComponent implements OnInit, OnDestroy {
   public navigation: {} = [
     {
