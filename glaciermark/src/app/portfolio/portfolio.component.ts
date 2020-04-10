8d96ba18212113f19d8b1793305a8260b70756c3
Tammy Dresen <dre15003@byui.edu>
2020-04-10 13:37:54 -0700

    added sitemap and robots.txt

--- glaciermark/src/app/portfolio/portfolio.component.ts
+++ glaciermark/src/app/portfolio/portfolio.component.ts
@@ -1,3 +1,4 @@
+import { SeoConfig } from './../services/seo/seo.interface';
 import { ViewportScroller } from '@angular/common';
 import { DataService } from './../services/data/data.service';
 import { Component, OnInit } from '@angular/core';
@@ -13,18 +14,19 @@ export class PortfolioComponent implements OnInit {
 
   public projects: any = [];
   public gallery: string = 'placeholder';
+  private data: SeoConfig = {
+    title: 'Print Design, Digital Design, Marketing, Branding, Consulting by Glacier Marketing Idaho Falls',
+      // tslint:disable-next-line: max-line-length
+      description: "Check out our portfolio of print design, digital &amp; website design, marketing or branding services. We have the experience to help your business with any marketing needs - all in one team! Don't parsel out your business marketing when you can get the Glacier Marketing services from one company. Call today 208-557-9114.",
+      url: 'https://glaciermark.com/portfolio'
+  };
 
   public constructor(
     private seo: SeoService,
     public dataService: DataService,
     private scroll: ViewportScroller
     ) {
-    this.seo.update({
-      title: 'Print Design, Digital Design, Marketing, Branding, Consulting by Glacier Marketing Idaho Falls',
-      // tslint:disable-next-line: max-line-length
-      description: "Check out our portfolio of print design, digital &amp; website design, marketing or branding services. We have the experience to help your business with any marketing needs - all in one team! Don't parsel out your business marketing when you can get the Glacier Marketing services from one company. Call today 208-557-9114.",
-      url: 'https://glaciermark.com/portfolio'
-    });
+    this.seo.update(this.data);
   }
 
   public ngOnInit() {
